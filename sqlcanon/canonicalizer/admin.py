6bafc239c7298b79bffdab0cfa58a94edbaf6a8a
Elmer Medez <EMedez@palominodb.com>
2013-02-16 00:55:23 +0800

    Minor UI changes.

--- sqlcanon/canonicalizer/admin.py
+++ sqlcanon/canonicalizer/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from canonicalizer.models import CapturedStatement, CanonicalizedStatement
 
 class CanonicalizedStatementAdmin(admin.ModelAdmin):
-    list_display = ('id', 'statement', 'hostname', 'hash',
-                    'statement_hostname_hash', 'instances')
+    list_display = ('id', 'statement', 'hostname', 'hash_as_hex_str',
+                    'statement_hostname_hash_as_hex_str', 'instances')
 
 class CapturedStatementAdmin(admin.ModelAdmin):
     list_display = ('id', 'dt', 'statement', 'hostname',
                     'canonicalized_statement',
-                    'canonicalized_statement_hash',
-                    'statement_hostname_hash',
+                    'canonicalized_statement_hash_as_hex_str',
+                    'statement_hostname_hash_as_hex_str',
                     'sequence_id', 'last_updated')
 
 admin.site.register(CanonicalizedStatement, CanonicalizedStatementAdmin)
